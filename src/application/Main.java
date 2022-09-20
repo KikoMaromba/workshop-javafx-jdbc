c573e19ecd036884262793ebf2de86f5553df0cf
KikoMaromba <Kaiquebragasantos@gmail.com>
2022-09-20 12:11:47 -0300

    About view implementado, Classes Constraints e Alerts implementadas

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -16,13 +21,17 @@ public class Main extends Application {
 			scroll.setFitToHeight(true);
 			scroll.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scroll);
+			mainScene = new Scene(scroll);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
